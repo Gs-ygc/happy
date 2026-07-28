996c860d340462ed18f0c9aee45a693bf2f8133c
Gs-ygc <gs2ygc@gmail.com>
2026-07-28 14:59:09 +0800

    release: bump production app to 1.7.1 (141)

--- packages/happy-app/app.config.js
+++ packages/happy-app/app.config.js
@@ -59,7 +59,7 @@ export default {
     expo: {
         name,
         slug: "happy",
-        version: "1.7.0",
+        version: "1.7.1",
         runtimeVersion: "21",
         orientation: "default",
         icon: "./sources/assets/images/icon.png",
@@ -92,6 +92,7 @@ export default {
                 : {})
         },
         android: {
+            versionCode: 141,
             adaptiveIcon: {
                 foregroundImage: "./sources/assets/images/icon-adaptive.png",
                 monochromeImage: "./sources/assets/images/icon-monochrome.png",
